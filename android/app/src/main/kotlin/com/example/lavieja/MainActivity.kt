dd07534130ec55ccc5d1a82989a6f41116cc4e10
javidiazrve <68872873+javidiazrve@users.noreply.github.com>
2023-06-30 13:30:32 -0300

    app initialized

---
+++ android/app/src/main/kotlin/com/example/lavieja/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lavieja
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/util/mainColors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MainColors {
+  static Color primary = Colors.white;
+  static Color secundary = Colors.grey;
+  static Color background = Colors.blueGrey.shade900;
+  static Color foreground = Colors.black87;
+}
